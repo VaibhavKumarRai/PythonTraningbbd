e01c0e1a50888218244ac7dd1222b10830f9b736
VaibhavKumarRai <raivaibhav64@bbdu.ac.in>
2026-01-14 11:58:49 +0530

    First commit

---
+++ traning file.py
@@ -0,0 +1,10 @@
+
+Pen_price = 15
+Notebook_price = 40
+
+pen_qty = int(input("Enter number of pens: "))
+notebook_qty = int(input("Enter number of notebooks: "))
+
+Total_Cost = (Pen_price * pen_qty) + (Notebook_price * notebook_qty)
+
+print("The total cost of your purchase is:", Total_Cost)
